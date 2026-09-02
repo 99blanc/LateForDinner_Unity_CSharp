219995dde7410cc9ea3ed12147397c4eb5e397d2
99blanc <t990421@naver.com>
2026-09-02 23:49:55 +0900

    [Chore] Roll 상태 수정

--- LateForDinner/Assets/Scripts/Characters/Interfaces/IRollableCharacter.cs
+++ LateForDinner/Assets/Scripts/Characters/Interfaces/IRollableCharacter.cs
@@ -12,5 +12,27 @@ public interface IRollableCharacter
             return;
 
         Renderer.FlipX(directionX);
+        float maxMoveSpeed = Attributes.Get<float>(AttributeType.MoveSpeed).CurrentValue;
+        float acceleration = Attributes.Get<float>(AttributeType.Acceleration).CurrentValue;
+        float deceleration = Attributes.Get<float>(AttributeType.Deceleration).CurrentValue;
+        float turnDeceleration = Attributes.Get<float>(AttributeType.TurnDeceleration).CurrentValue;
+        float targetVelocityX = directionX * maxMoveSpeed;
+        float currentVelocityX = Rigidbody.linearVelocity.x;
+        float rate = DetermineRate(currentVelocityX, directionX, acceleration, deceleration, turnDeceleration);
+        float newVelocityX = Mathf.MoveTowards(currentVelocityX, targetVelocityX, rate * Time.fixedDeltaTime);
+        Vector2 velocity = Rigidbody.linearVelocity;
+        velocity.x = newVelocityX;
+        Rigidbody.linearVelocity = velocity;
+    }
+
+    private float DetermineRate(float currentVelocityX, float directionX, float acceleration, float deceleration, float turnDeceleration)
+    {
+        if (Mathf.Abs(directionX) > 0.01f)
+        {
+            bool isTurning = Mathf.Sign(currentVelocityX) != Mathf.Sign(directionX) && Mathf.Abs(currentVelocityX) > 0.1f;
+            return isTurning ? turnDeceleration : acceleration;
+        }
+
+        return deceleration;
     }
 }
